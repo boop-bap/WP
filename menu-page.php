d926727e5d3db2e812f6df3807bebacd71ad0e14
Justas Kavarskas <justask41@gmail.com>
2022-01-09 12:52:32 +0100

    Menu page top created

--- menu-page.php
+++ menu-page.php
@@ -4,10 +4,100 @@
 <?php get_header(); ?>
 
 <body>
-    <h1>Custom page</h1>
-    <h1>Custom page</h1>
+    <main>
+        <section class="burger-menu">
+            <h1>THE BURGER MENU</h1>
+            <div class="burger-menu-container">
+                <a href="#" class="burger-menu_item">
+                    <img src=<?php echo get_theme_file_uri('./images/burger1.jpg') ?> alt="burger menu item">
+                    <h4>THE SPECIAL MENU</h4>
+                    <div class="price-container">
+                        <i class="price-icon price-icon_extra ">99kr.-</i>
+                        <i class="price-icon_secondary">54kr. <br> SINGLE</i>
+                    </div>
+                </a>
+                <a href="#" class="burger-menu_item">
+                    <img src=<?php echo get_theme_file_uri('./images/burger1.jpg') ?> alt="burger menu item">
+                    <h4>THE SPECIAL MENU</h4>
+                    <div class="price-container">
+                        <i class="price-icon price-icon_extra">99kr.-</i>
+                        <i class="price-icon_secondary">54kr. <br> SINGLE</i>
+                    </div>
+                </a>
+                <a href="#" class="burger-menu_item">
+                    <img src=<?php echo get_theme_file_uri('./images/burger1.jpg') ?> alt="burger menu item">
+                    <h4>THE SPECIAL MENU</h4>
+                    <div class="price-container">
+                        <i class="price-icon price-icon_extra">99kr.-</i>
+                        <i class="price-icon_secondary">54kr. <br> SINGLE</i>
+                    </div>
+                </a>
+                <a href="#" class="burger-menu_item">
+                    <img src=<?php echo get_theme_file_uri('./images/burger1.jpg') ?> alt="burger menu item">
+                    <h4>THE SPECIAL MENU</h4>
+                    <div class="price-container">
+                        <i class="price-icon price-icon_extra">99kr.-</i>
+                        <i class="price-icon_secondary">54kr. <br> SINGLE</i>
+                    </div>
+                </a>
+                <a href="#" class="burger-menu_item">
+                    <img src=<?php echo get_theme_file_uri('./images/burger1.jpg') ?> alt="burger menu item">
+                    <h4>THE SPECIAL MENU</h4>
+                    <div class="price-container">
+                        <i class="price-icon price-icon_extra">99kr.-</i>
+                        <i class="price-icon_secondary">54kr. <br> SINGLE</i>
+                    </div>
+                </a>
+            </div>
 
-    <h1>Custom page</h1>
+        </section>
+
+
+
+
+        <section class="special-offers">
+            <h2>SPECIAL DEALS</h2>
+            <div class="special-offers-container">
+                <a href='' class="special-offers_burger">
+                    <img src=<?php echo get_theme_file_uri('./images/burger.png') ?> alt="special offer burger">
+                    <h4>2x the special</h4>
+                    <p>Cheese, bacon, 100% beef
+                        tomato, cheese, chili mayo, lettuce, onion, red onion, jalapeño, gravy, red peppers</p>
+                    <div class="price-container">
+                        <i class="price-icon">99kr.-</i>
+                        <p>save 9kr.</p>
+                    </div>
+                </a>
+                <a href='' class="special-offers_burger">
+                    <img src=<?php echo get_theme_file_uri('./images/burger.png') ?> alt="special offer burger">
+                    <h4>2x the special</h4>
+                    <p>Cheese, bacon, 100% beef
+                        tomato, cheese, chili mayo, lettuce, onion, red onion, jalapeño, gravy, red peppers</p>
+                    <div class="price-container">
+                        <i class="price-icon">99kr.-</i>
+                        <p>save 9kr.</p>
+                    </div>
+                </a>
+                <a href='' class="special-offers_burger">
+                    <img src=<?php echo get_theme_file_uri('./images/burger.png') ?> alt="special offer burger">
+                    <h4>2x the special</h4>
+                    <p>Cheese, bacon, 100% beef
+                        tomato, cheese, chili mayo, lettuce, onion, red onion, jalapeño, gravy, red peppers</p>
+                    <div class="price-container">
+                        <i class="price-icon">99kr.-</i>
+                        <p>save 9kr.</p>
+                    </div>
+                </a>
+            </div>
+        </section>
+
+
+
+
+
+    </main>
+    <aside>Aside</aside>
+
 
 
 
