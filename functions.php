ebd3c47bfec1955e325f544bf12aae8a7ad16fe0
Justas Kavarskas <justask41@gmail.com>
2022-01-10 22:22:03 +0100

    working slide show

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function test_theme_files() {
-  wp_enqueue_script('main-university-js', get_theme_file_uri('/src/index.js'), array(''), '1.0', true);
+  wp_enqueue_script('main-university-js', get_theme_file_uri('./src/index.js'), array('jquery'), '1.0', true);
   wp_enqueue_script( 'fa', 'https://kit.fontawesome.com/191dcb4902.js', array(), '1.0.0', true );
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;600;700;900');
   wp_enqueue_style('custom-google-fonts-two', '//fonts.googleapis.com/css2?family=Overpass:wght@300;400;500;600;700;800;900');
