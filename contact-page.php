d6e7d6dd03983fb6ac7888b2eeea62a52c4ea0a9
DominikRostek <1089269@ucn.dk>
2022-01-10 21:26:19 +0100

    made contact page and indiviudal burger page functional

--- contact-page.php
+++ contact-page.php
@@ -4,11 +4,49 @@
 <?php get_header(); ?>
 
 <body>
-    <h1>Custom page</h1>
-    <h1>Custom page</h1>
+    <section class="contact-page">
+        <h1>CONTACT</h1>
+    <div class="contact-intro">
+        
+        <p>Burger Go is located near Jomfru Ane Gade in the centre of Aalborg. If you have any questions or would like
+            to place an order, you are welcome to drop by the restaurant or simply give us a call.</p>
+</div>
+        <div class="contact-info">
+        <h3>GET IN TOUCH</h3> <br>
+        <p>Burger Go<br>
+            Ved stranden 11B<br>
+            9000 Aalborg<br>
+        </p><br>
+        <p>Tel: +45 52 95 65 78<br>
+        </p>
+        <br>
+        <a class="directions" href="https://www.google.com/maps/dir//Burger+GO+Ved+Stranden+11B+9000+Aalborg/@57.050467,9.920234,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x4649328c63bf7415:0x7c6478aac36f1a84!2m2!1d9.920234!2d57.050467">GET DIRECTIONS</a>
+    </div>
+    <div class="vertical">
+    </div>
 
-    <h1>Custom page</h1>
-
-
-
+    <div class="contact-hours">
+        <h3>OPENING HOURS</h3>
+        <p>Mon - Thu <br>
+            16:30 - 21:00
+        </p>
+        <br>
+        <p>Friday <br>
+            11:30 - 21:30
+        </p>
+        <br>
+        <p>Saturday<br>
+            11:30 - 21:00
+        </p>
+        <br>
+        <p>Sunday<br>
+            12:00 - 21:00
+        </p>
+</div>
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2170.062453759092!2d9.918045315898109!3d57.05046989862971!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4649328c63bf7415%3A0x7c6478aac36f1a84!2sBurger%20GO!5e0!3m2!1sda!2sdk!4v1641657272732!5m2!1sda!2sdk"
+            width="100%" height="100%" style="border-radius: 15px; border: none;" allowfullscreen="">
+        </iframe>
+    
+    </section>
     <?php get_footer();?>
---
+++ single.php
@@ -0,0 +1,78 @@
+<?php /* Template Name: Single Page */ ?>
+
+
+<?php get_header(); ?>
+
+<body>
+    <a href="<?php echo site_url('/menu')?>">
+        <div class="back-home">
+            <i class="fas fa-chevron-left"></i>
+            <h3>BACK TO MENU</h3>
+        </div>
+    </a>
+
+    <div class="product-card">
+        <img src=<?php the_field("product_photo"); ?> alt="burger menu item">
+        <h1><?php the_field("product_name"); ?></h1>
+        <div class="desc">
+            <p><?php the_field("description"); ?></p>
+        </div>
+        <div class="prices">
+            <i class="price-icon price-icon_extra "><?php the_field("price"); ?> Kr.</i>
+            <i class="price-icon_secondary"><?php the_field("menu_price"); ?> Kr.<br> SINGLE</i>
+            <p>GET 2 SINGLES ADD 40 Kr.</p>
+        </div>
+    </div>
+
+    <hr class="horizonal">
+
+    <div class="add-options">
+        <h2>ADD SIDES AND DRINKS TO YOUR ORDER</h2>
+        <div class="item1">
+            <img src=<?php echo get_theme_file_uri('./images/Fries-min.jpeg') ?> alt="burger menu item">
+            <h4>FRIES</h4>
+            <i class="price-icon price-icon_extra ">32kr.-</i>
+        </div>
+        <div class="item2"><img src=<?php echo get_theme_file_uri('./images/Drinks.png') ?> alt="burger menu item">
+            <h4>DRINKS</h4>
+            <i class="price-icon price-icon_extra ">23kr.-</i>
+        </div>
+        <div class="item3"><img src=<?php echo get_theme_file_uri('./images/Hot_wings-min.jpeg') ?>
+                alt="burger menu item">
+            <h4>HOT WINGS</h4>
+            <i class="price-icon price-icon_extra ">35kr.-</i>
+        </div>
+    </div>
+    <hr class="horizonal">
+
+    <div class="add-options">
+        <h2>ADD SIDES AND DRINKS TO YOUR ORDER</h2>
+        <div class="burger-menu-container">
+
+
+            <?php $menuPosts = new WP_Query (array(
+            "category_name" => "burgers",
+            "posts_per_page"=> 3
+        ));
+        while ($menuPosts -> have_posts()) {
+            $menuPosts -> the_post(); ?>
+
+
+<a href="<?php the_permalink(); ?>">
+            <div class="product-item burger-menu_item">
+                
+                    <img src="<?php the_field("product_photo")?>">
+                    <h4><?php the_field("product_name"); ?></h4>
+                    <div class="price-container">
+                        <i class="price-icon price-icon_extra "><?php the_field("price"); ?> Kr.</i>
+                        <i class="price-icon_secondary"><?php the_field("menu_price"); ?> Kr.<br> SINGLE</i>
+
+                    </div>
+                
+            </div>
+</a>
+            <?php } ?>
+        </div>
+
+    </div>
+    <?php get_footer();?>
