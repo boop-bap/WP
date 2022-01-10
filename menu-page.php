d3ab356f66a0d53fdb90d429f9d6bd60a8d25556
DominikRostek <1089269@ucn.dk>
2022-01-10 14:19:11 +0100

    posts are displaying

--- menu-page.php
+++ menu-page.php
@@ -5,96 +5,150 @@
 
 <body>
     <main>
+
+        <?php 
+
+			$photo = get_field('product_photo');
+
+		?>
+
         <section class="burger-menu">
             <h1>THE BURGER MENU</h1>
             <div class="burger-menu-container">
-                <a href="<?php echo site_url('/single-product')?>" class="burger-menu_item">
-                    <img src=<?php echo get_theme_file_uri('./images/burger1.jpg') ?> alt="burger menu item">
-                    <h4>THE SPECIAL MENU</h4>
-                    <div class="price-container">
-                        <i class="price-icon price-icon_extra ">99kr.-</i>
-                        <i class="price-icon_secondary">54kr. <br> SINGLE</i>
-                    </div>
-                </a>
-                <a href="#" class="burger-menu_item">
-                    <img src=<?php echo get_theme_file_uri('./images/burger1.jpg') ?> alt="burger menu item">
-                    <h4>THE SPECIAL MENU</h4>
-                    <div class="price-container">
-                        <i class="price-icon price-icon_extra">99kr.-</i>
-                        <i class="price-icon_secondary">54kr. <br> SINGLE</i>
-                    </div>
-                </a>
-                <a href="#" class="burger-menu_item">
-                    <img src=<?php echo get_theme_file_uri('./images/burger1.jpg') ?> alt="burger menu item">
-                    <h4>THE SPECIAL MENU</h4>
-                    <div class="price-container">
-                        <i class="price-icon price-icon_extra">99kr.-</i>
-                        <i class="price-icon_secondary">54kr. <br> SINGLE</i>
-                    </div>
-                </a>
-                <a href="#" class="burger-menu_item">
-                    <img src=<?php echo get_theme_file_uri('./images/burger1.jpg') ?> alt="burger menu item">
-                    <h4>THE SPECIAL MENU</h4>
-                    <div class="price-container">
-                        <i class="price-icon price-icon_extra">99kr.-</i>
-                        <i class="price-icon_secondary">54kr. <br> SINGLE</i>
-                    </div>
-                </a>
-                <a href="#" class="burger-menu_item">
-                    <img src=<?php echo get_theme_file_uri('./images/burger1.jpg') ?> alt="burger menu item">
-                    <h4>THE SPECIAL MENU</h4>
-                    <div class="price-container">
-                        <i class="price-icon price-icon_extra">99kr.-</i>
-                        <i class="price-icon_secondary">54kr. <br> SINGLE</i>
-                    </div>
-                </a>
+                <?php $menuPosts = new WP_Query (array(
+            "post_per_page" => 2,
+            "category_name" => "burgers"
+        ));
+        while ($menuPosts -> have_posts()) {
+            $menuPosts -> the_post(); 
+        
+         ?>
+                <div class="product-item">
+                    <a href="<?php  ?>" class="burger-menu_item">
+                        <img src="<?php the_field("product_photo")?>" alt="burger menu item">
+                        <h4><?php the_field("product_name"); ?></h4>
+                        <div class="price-container">
+                            <i class="price-icon price-icon_extra "><?php the_field("price"); ?> Kr.</i>
+                            <i class="price-icon_secondary"><?php the_field("menu_price"); ?> Kr.<br> SINGLE</i>
+                    </a>
+                </div>
             </div>
+
+            <?php } ?>
 
         </section>
 
+        <hr class="horizonal">
 
-
-
-        <section class="special-offers">
-            <h2>SPECIAL DEALS</h2>
-            <div class="special-offers-container">
-                <a href='' class="special-offers_burger">
-                    <img src=<?php echo get_theme_file_uri('./images/burger.png') ?> alt="special offer burger">
-                    <h4>2x the special</h4>
-                    <p>Cheese, bacon, 100% beef
-                        tomato, cheese, chili mayo, lettuce, onion, red onion, jalapeño, gravy, red peppers</p>
-                    <div class="price-container">
-                        <i class="price-icon">99kr.-</i>
-                        <p>save 9kr.</p>
-                    </div>
-                </a>
-                <a href='' class="special-offers_burger">
-                    <img src=<?php echo get_theme_file_uri('./images/burger.png') ?> alt="special offer burger">
-                    <h4>2x the special</h4>
-                    <p>Cheese, bacon, 100% beef
-                        tomato, cheese, chili mayo, lettuce, onion, red onion, jalapeño, gravy, red peppers</p>
-                    <div class="price-container">
-                        <i class="price-icon">99kr.-</i>
-                        <p>save 9kr.</p>
-                    </div>
-                </a>
-                <a href='' class="special-offers_burger">
-                    <img src=<?php echo get_theme_file_uri('./images/burger.png') ?> alt="special offer burger">
-                    <h4>2x the special</h4>
-                    <p>Cheese, bacon, 100% beef
-                        tomato, cheese, chili mayo, lettuce, onion, red onion, jalapeño, gravy, red peppers</p>
-                    <div class="price-container">
-                        <i class="price-icon">99kr.-</i>
-                        <p>save 9kr.</p>
-                    </div>
-                </a>
+        <section class="burger-menu">
+            <h1>SPECIAL OFFERS</h1>
+            <div class="burger-menu-container">
+                <?php $menuPosts = new WP_Query (array(
+            "post_per_page" => 2,
+            "category_name" => "offers"
+        ));
+        while ($menuPosts -> have_posts()) {
+            $menuPosts -> the_post(); 
+        
+         ?>
+                <div class="product-item">
+                    <a href="<?php the_permalink(); ?>" class="burger-menu_item">
+                        <img src=<?php the_field("product_photo"); ?> alt="burger menu item">
+                        <h4><?php the_field("product_name"); ?></h4>
+                        <div class="price-container">
+                            <i class="price-icon price-icon_extra "><?php the_field("price"); ?> Kr.</i>
+                            <i class="price-icon_secondary"><?php the_field("menu_price"); ?> Kr.<br> SINGLE</i>
+                    </a>
+                </div>
             </div>
+
+            <?php } ?>
+
         </section>
 
+        <hr class="horizonal">
 
+        <section class="burger-menu">
+            <h1>SIDE DISHES</h1>
+            <div class="burger-menu-container">
+                <?php $menuPosts = new WP_Query (array(
+            "post_per_page" => 2,
+            "category_name" => "side_dishes"
+        ));
+        while ($menuPosts -> have_posts()) {
+            $menuPosts -> the_post(); 
+        
+         ?>
+                <div class="product-item">
+                    <a href="<?php echo site_url('/single-product')?>" class="burger-menu_item">
+                        <img src=<?php the_field("product_photo"); ?> alt="burger menu item">
+                        <h4><?php the_field("product_name"); ?></h4>
+                        <div class="price-container">
+                            <i class="price-icon price-icon_extra "><?php the_field("price"); ?> Kr.</i>
+                            <i class="price-icon_secondary"><?php the_field("menu_price"); ?> Kr.<br> SINGLE</i>
+                    </a>
+                </div>
+            </div>
 
+            <?php } ?>
+
+        </section>
+
+        <hr class="horizonal">
+
+        <section class="burger-menu">
+            <h1>DIPS</h1>
+            <div class="burger-menu-container">
+                <?php $menuPosts = new WP_Query (array(
+            "post_per_page" => 2,
+            "category_name" => "dips"
+        ));
+        while ($menuPosts -> have_posts()) {
+            $menuPosts -> the_post(); 
+        
+         ?>
+                <div class="product-item">
+                    <a href="<?php echo site_url('/single-product')?>" class="burger-menu_item">
+                        <img src=<?php the_field("product_photo"); ?> alt="burger menu item">
+                        <h4><?php the_field("product_name"); ?></h4>
+                        <div class="price-container">
+                            <i class="price-icon price-icon_extra "><?php the_field("price"); ?> Kr.</i>
+                            <i class="price-icon_secondary"><?php the_field("menu_price"); ?> Kr.<br> SINGLE</i>
+                    </a>
+                </div>
+            </div>
+
+            <?php } ?>
+
+        </section>
+        <hr class="horizonal">
+
+        <section class="burger-menu">
+            <h1>DRINKS</h1>
+            <div class="burger-menu-container">
+                <?php $menuPosts = new WP_Query (array(
+            "post_per_page" => 2,
+            "category_name" => "drinks"
+        ));
+        while ($menuPosts -> have_posts()) {
+            $menuPosts -> the_post(); 
+        
+         ?>
+                <div class="product-item">
+                    <a href="<?php echo site_url('/single-product')?>" class="burger-menu_item">
+                        <img src=<?php the_field("product_photo"); ?> alt="burger menu item">
+                        <h4><?php the_field("product_name"); ?></h4>
+                        <div class="price-container">
+                            <i class="price-icon price-icon_extra "><?php the_field("price"); ?> Kr.</i>
+                            <i class="price-icon_secondary"><?php the_field("menu_price"); ?> Kr.<br> SINGLE</i>
+                    </a>
+                </div>
+            </div>
+
+            <?php } ?>
+
+        </section>
     </main>
-    <aside>Aside</aside>
 
 
 
