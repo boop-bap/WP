52b6340629c48baaa95bb261468954f6ce9a2016
DominikRostek <1089269@ucn.dk>
2022-01-12 12:29:53 +0100

    small-adjustments

--- header.php
+++ header.php
@@ -15,19 +15,19 @@
                     <a href="<?php echo site_url('/')?>" class="logo"><img
                             src=<?php echo get_theme_file_uri('./images/logo.png') ?> alt="logo"></a>
                     <ul>
-                        <li><a href="<?php echo site_url('/')?>">Home</a></li>
-                        <li><a href="<?php echo site_url('/menu')?>">Menu</a></li>
-                        <li><a href="<?php echo site_url('/about-page')?>">About</a></li>
-                        <li><a href="<?php echo site_url('/contact')?>">Contact</a></li>
+                        <li><a href="<?php echo site_url('/')?>">HOME</a></li>
+                        <li><a href="<?php echo site_url('/menu')?>">MENU</a></li>
+                        <li><a href="<?php echo site_url('/about-page')?>">ABOUT</a></li>
+                        <li><a href="<?php echo site_url('/contact')?>">CONTACT</a></li>
                     </ul>
                     <div class="dropdown">
                         <button class="dropbtn">ORDERING OPTIONS</button>
                         <div id="myDropdown" class="dropdown-content">
                             <div class="dropdown-container">
                                 <div class="dropdown-container_details">
-                                    <p>+45 52 62 58 89</p>
-                                    <p>Call now</p>
-                                    <p>Icon</p>
+                                    <a href="tel:+45 60141520"><p id="copynumber" value="+45 52 62 58 89">+45 52 62 58 89</p></a>
+                                    <a href="tel:+45 60141520"><p>Call now</p></a>
+                                    <i onclick="copy()"><i class="far fa-copy"></i></i>
                                 </div>
                                 <div class="dropdown-container_divider">
                                     <div class="dropdown-line"></div>
@@ -36,7 +36,7 @@
                                 </div>
 
                                 <div class="dropdown-container_services">
-                                    <img src=<?php echo get_theme_file_uri('./images/hungry.svg') ?> alt=" hungry">
+                                    <img src=<?php echo get_theme_file_uri('./images/hungry.svg') ?> alt=" hungry" style="zoom: 2; margin-left: -30px;">
                                     <img src=<?php echo get_theme_file_uri('./images/waiteer.svg') ?> alt=" waiteer">
                                     <img src=<?php echo get_theme_file_uri('./images/wolt.svg') ?> alt="wolt">
                                     <img src=<?php echo get_theme_file_uri('./images/justeat.svg') ?> alt="justeat">
