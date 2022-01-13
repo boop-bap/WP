92e68e6ec575ec3ee096085e5242892d41e3785b
Justas Kavarskas <justask41@gmail.com>
2022-01-13 06:00:36 +0100

    fixed dropdown

--- header.php
+++ header.php
@@ -61,10 +61,21 @@
                                 </div>
 
                                 <div class="dropdown-container_services">
-                                    <img src=<?php echo get_theme_file_uri('./images/hungry.svg') ?> alt=" hungry">
-                                    <img src=<?php echo get_theme_file_uri('./images/waiteer.svg') ?> alt=" waiteer">
-                                    <img src=<?php echo get_theme_file_uri('./images/wolt.svg') ?> alt="wolt">
-                                    <img src=<?php echo get_theme_file_uri('./images/justeat.svg') ?> alt="justeat">
+                                <a
+                                        href="https://www.hungry.dk/aalborg/burger-go/?gclid=EAIaIQobChMItMydhpGs9QIVaI1oCR1OlAFjEAAYASAAEgLTGPD_BwE">
+                                        <img src=<?php echo get_theme_file_uri('./images/hungry.svg') ?> alt=" hungry">
+                                    </a>
+                                    <a href="https://waiteer.com/restaurant/aalborg/burger-go"> <img
+                                            src=<?php echo get_theme_file_uri('./images/waiteer.svg') ?> alt=" waiteer">
+                                    </a>
+                                    <a
+                                        href="https://www.just-eat.dk/menu/burger-go?k1111=k1111&gclid=EAIaIQobChMIn8ndk5Ss9QIVkI9oCR3yHAWDEAAYAiAAEgLqyvD_BwE&gclsrc=aw.ds">
+                                        <img src=<?php echo get_theme_file_uri('./images/justeat.svg') ?> alt="justeat">
+                                    </a>
+                                    <a
+                                        href="https://wolt.com/da/DNK/aalborg/restaurant/burger-go?utm_source=google&utm_medium=cpc&utm_term=burger%20go&utm_campaign=DNK_Web_FTU_Search_Restaurant-venue_Aalborg_DA&gclid=EAIaIQobChMIn8ndk5Ss9QIVkI9oCR3yHAWDEAAYAyAAEgJNH_D_BwE">
+                                        <img src=<?php echo get_theme_file_uri('./images/wolt.svg') ?> alt="wolt">
+                                    </a>
                                 </div>
                             </div>
                         </div>
